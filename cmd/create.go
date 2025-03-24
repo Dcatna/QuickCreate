21e441eab9dc71fc7c834a1e0d98f38574ff4669
Dcatna <ddcatena@gmail.com>
2025-03-23 20:51:52 -0400

    create basic scaffolding commands for react apps

--- cmd/create.go
+++ cmd/create.go
@@ -1,28 +1,57 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 )
 
+var projectName string
+
 // createCmd represents the create command
 var createCmd = &cobra.Command{
-	Use:   "create",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "create [template]",
+	Short: "Create a new project using a predefined template",
+	Long: `Scaffold a project from a built in template like react-app-js or react-app-ts`,
+	Args: cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("create called")
+		template := args[0]
+
+		if projectName == "" {
+			fmt.Println("Please provide a project name with --name")
+			return
+		}
+
+		fmt.Printf("Creating %s using template %s\n", projectName, template)
+
+		switch template {
+		case "react-app-js":
+			runCommand("npm", "create", "vite@latest", projectName, "--", "--template", "react")
+		case "react-app-ts":
+			runCommand("npm", "create", "vite@latest", projectName, "--", "--template", "react-ts")
+
+		default:
+			fmt.Println("Unknown template, do --help for prefdefined templates")
+		}
 	},
+}
+
+func runCommand(name string, args ...string) {
+	cmd := exec.Command(name, args...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+
+	err := cmd.Run()
+	if err != nil {
+		fmt.Printf("Command failed %v\n", err)
+	}
 }
 
 func init() {
@@ -37,4 +66,7 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	rootCmd.AddCommand(createCmd)
+	createCmd.Flags().StringVarP(&projectName, "name", "n", "", "Name of the project folder")
 }
